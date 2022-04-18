259321e8525dfde672dfc323dc9cedd7cd0011a9
beletate <vinicius_beletate@hotmail.com>
2022-04-18 16:28:21 -0300

    first commit

---
+++ src/main/java/univas/edu/vinicius/ViniciusApplication.java
@@ -0,0 +1,28 @@
+package univas.edu.vinicius;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import univas.edu.vinicius.DAO.ProductDAO;
+import univas.edu.vinicius.model.Product;
+
+@SpringBootApplication
+public class ViniciusApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViniciusApplication.class, args);
+	}
+
+	@Autowired
+	private ProductDAO product;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Product product_1 = new Product("SSD", "Armazenador de Arquivos", 400, 1);
+		product.save(product_1);
+
+		Product product_2 = new Product("HD", "Armazenador de Arquivos", 100, 1);
+		product.save(product_2);
+	}
+}
